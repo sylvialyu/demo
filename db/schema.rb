ae396fdc7ef4267432aa88f2f3e7d6c93232ebdd
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-02 21:06:55 +0800

    create course model for future usage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802082809) do
+ActiveRecord::Schema.define(version: 20170802122019) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
