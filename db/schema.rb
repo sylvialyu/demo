37c77b5a6107a7cc435167dbec6487d12f4108b7
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-18 14:06:12 +0800

    admin can upload photo in the backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812121814) do
+ActiveRecord::Schema.define(version: 20170818121404) do
 
   create_table "challenges", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20170812121814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "level_id"
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "courses", force: :cascade do |t|
