54ff51d8e69e1eb36db1039fd8b3eef221472128
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-31 14:51:33 +0800

    finish landing page - banner except button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def avatar_url(user, size)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=wavatar"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
   end
 
   def render_user_avatar(user, size)
